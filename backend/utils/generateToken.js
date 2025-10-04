976d5a76bff1954ac3bfe7e0ae44f06ab6be740b
Ankush22vrm <vermaankush589@gmail.com>
2025-10-04 23:30:00 +0530

    api testing on postman

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const generateToken = (userId) => jwt.sign({ id: userId }, process.env.JWT_SECRET, {
-  expiresIn: '30d',
+  expiresIn: '10d',
 });
 
 module.exports = generateToken;
